01fdfc6c9f8e093e62287213059cceff54aa6494
Zhendong Tang <zhendongtt@gmail.com>
2021-12-10 06:11:02 -0500

    20-005 Working with Links
    Link relys on Route to work
    if comment those routes, Link cannot find relevant component

--- starting-project/src/App.js
+++ starting-project/src/App.js
@@ -1,13 +1,17 @@
 import { Routes, Route } from "react-router-dom";
-import Welcome from "./components/Welcome";
-import Products from "./components/Products";
+import Welcome from "./pages/Welcome";
+import Products from "./pages/Products";
+import About from "./pages/About";
+import MainHeader from "./components/MainHeader";
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
+      <MainHeader />
       <Routes>
         <Route path="/welcome" element={<Welcome />} />
         <Route path="/products" element={<Products />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </div>
   );
---
+++ starting-project/src/components/MainHeader.js
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+
+import classes from "./MainHeader.module.css";
+
+const MainHeader = () => {
+  return (
+    <header className={classes.header}>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/welcome">Welcome</Link>
+          </li>
+          <li>
+            <Link to="/products">Products</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default MainHeader;
